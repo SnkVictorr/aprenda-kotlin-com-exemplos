8d72c320bd13ff1f62d45c21f02d310d53761ab9
SnkVictorr <99307258+SnkVictorr@users.noreply.github.com>
2022-12-27 17:21:03 -0300

    Update exemplo1.kt

--- 2_controle_de_fluxo/5_expressoes_condicionais/exemplo1.kt
+++ 2_controle_de_fluxo/5_expressoes_condicionais/exemplo1.kt
@@ -4,5 +4,7 @@
  * @see [Conditional Expression](https://play.kotlinlang.org/byExample/02_control_flow/05_Conditional%20expression)
  */
 fun main() {
-    
+    fun max(a: Int, b: Int) = if (a > b) a else b         // 1 expressão if que verifica se a é maior que b, e retorna a se for, e b se n for
+
+    println(max(99, -42))
  }
