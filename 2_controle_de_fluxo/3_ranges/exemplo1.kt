47111abe64a752f71308bbcaf3a9b40820c2eb01
SnkVictorr <99307258+SnkVictorr@users.noreply.github.com>
2022-12-27 17:03:25 -0300

    Update exemplo1.kt

--- 2_controle_de_fluxo/3_ranges/exemplo1.kt
+++ 2_controle_de_fluxo/3_ranges/exemplo1.kt
@@ -3,6 +3,26 @@
  *
  * @see [Ranges](https://play.kotlinlang.org/byExample/02_control_flow/03_Ranges)
  */
+
 fun main() {
-    
+    for(i in 0..3) {             // 1 vai de 0 até 3 inclusive
+        print(i)
+    }
+    print(" ")
+
+    for(i in 0 until 3) {        // 2 vai de 0 até menor que 3 exclusive
+        print(i)
+    }
+    print(" ")
+
+    for(i in 2..8 step 2) {      // 3 vai de 2 a 8 em 2 em 2
+        print(i)
+    }
+    print(" ")
+
+    for (i in 3 downTo 0) {      // 4 vai de 3 até 0
+         print(i)
+    }
+    print(" ")
+
  }
