babbfb04295b209835f353be1497a993e5ca5535
SnkVictorr <99307258+SnkVictorr@users.noreply.github.com>
2022-12-27 20:04:40 -0300

    Update exemplo1.kt

--- 3_colecoes/4_filter_function/exemplo1.kt
+++ 3_colecoes/4_filter_function/exemplo1.kt
@@ -7,9 +7,9 @@ fun main() {
 
     val numbers = listOf(1, -2, 3, -4, 5, -6)      // 1
     
-    val positives = numbers.filter { x -> x > 0 }  // 2
+    val positives = numbers.filter { x -> x > 0 }  // 2 verifica se os numeros são positivos
     
-    val negatives = numbers.filter { it < 0 }      // 3
+    val negatives = numbers.filter { it < 0 }      // 3 verifica se os numeros são negativos
 
     println("Numbers: $numbers")
     println("Positive Numbers: $positives")
