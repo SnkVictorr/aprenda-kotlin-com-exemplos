4eb1de0370e997002cc8c9b67e00211483ca7089
SnkVictorr <99307258+SnkVictorr@users.noreply.github.com>
2022-12-27 17:42:51 -0300

    Update exemplo1.kt

--- 3_colecoes/1_list/exemplo1.kt
+++ 3_colecoes/1_list/exemplo1.kt
@@ -3,9 +3,9 @@
  *
  * @see [List](https://play.kotlinlang.org/byExample/05_Collections/01_List)
  */
-
-val systemUsers: MutableList<Int> = mutableListOf(1, 2, 3)        // 1
-val sudoers: List<Int> = systemUsers                              // 2
+//Listas são estruturas de dados ordernadas, que pode ser mudada entre mutavel e imutavel 
+val systemUsers: MutableList<Int> = mutableListOf(1, 2, 3)        // 1 //listOf() cria uma lista imutavel mutableListOf() cria uma lista mutavel
+val sudoers: List<Int> = systemUsers                              // 2 //Criando uma cópia de leitura da lista anterior
 
 fun addSystemUser(newUser: Int) {                                 // 3
     systemUsers.add(newUser)                      
@@ -16,8 +16,8 @@ fun getSysSudoers(): List<Int> {                                  // 4
 }
 
 fun main() {
-    addSystemUser(4)                                              // 5 
-    println("Tot sudoers: ${getSysSudoers().size}")               // 6
+    addSystemUser(4)                                              // 5 //adicionando um novo usuario no sistema
+    println("Tot sudoers: ${getSysSudoers().size}")               // 6 //o resultado é 4, pois o systemUsers ta refletindo no sudoers
     getSysSudoers().forEach {                                     // 7
         i -> println("Some useful info on user $i")
     }
