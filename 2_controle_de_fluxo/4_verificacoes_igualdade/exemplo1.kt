4f9f8899c2bd50b5de149f400b864d244f8e0b84
SnkVictorr <99307258+SnkVictorr@users.noreply.github.com>
2022-12-27 17:17:45 -0300

    Update exemplo1.kt

--- 2_controle_de_fluxo/4_verificacoes_igualdade/exemplo1.kt
+++ 2_controle_de_fluxo/4_verificacoes_igualdade/exemplo1.kt
@@ -4,5 +4,9 @@
  * @see [Equality Checks](https://play.kotlinlang.org/byExample/02_control_flow/04_Equality%20Checks)
  */
 fun main() {
-    
+    val authors = setOf("Shakespeare", "Hemingway", "Twain") //setOf não aceitam elementos duplicados, diferente da lista e não ordenam valores
+    val writers = setOf("Twain", "Shakespeare", "Hemingway")
+
+    println(authors == writers)   // 1 // vai dar true pq possuem são estruturas identicas em termos de dados
+    println(authors === writers)  // 2 // as referencias(endereços de memória) são false pq possui endereços  diferentes
  }
