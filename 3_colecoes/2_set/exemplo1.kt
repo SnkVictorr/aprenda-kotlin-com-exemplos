6a7aa57fcc2444dbb840188e1a28f5f5c86ddeda
SnkVictorr <99307258+SnkVictorr@users.noreply.github.com>
2022-12-27 19:37:05 -0300

    Update exemplo1.kt

--- 3_colecoes/2_set/exemplo1.kt
+++ 3_colecoes/2_set/exemplo1.kt
@@ -3,9 +3,9 @@
  *
  * @see [Set](https://play.kotlinlang.org/byExample/05_Collections/02_Set)
  */
-
-val openIssues: MutableSet<String> = mutableSetOf("uniqueDescr1", "uniqueDescr2", "uniqueDescr3") // 1
-
+//setOf() cria um conjunto imutavel e mutableSetOf() Cria um conjunto mutavel. Um modo só leitura oider usa utilizando Set
+val openIssues: MutableSet<String> = mutableSetOf("uniqueDescr1", "uniqueDescr2", "uniqueDescr3") // 1 Conjuntos são ordenados e n permitem duplicatas
+//MutableSet<String> n é obrigatorio, mas é usado pra determinar o tipo
 fun addIssue(uniqueDesc: String): Boolean {                                                       
     return openIssues.add(uniqueDesc)                                                             // 2
 }
