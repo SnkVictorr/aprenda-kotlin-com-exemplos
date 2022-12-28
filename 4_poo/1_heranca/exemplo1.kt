d1bee747314e32f6fc293c1fd785efeef75d2dd6
SnkVictorr <99307258+SnkVictorr@users.noreply.github.com>
2022-12-28 10:59:08 -0300

    Update exemplo1.kt

--- 4_poo/1_heranca/exemplo1.kt
+++ 4_poo/1_heranca/exemplo1.kt
@@ -3,15 +3,15 @@
  *
  * @see [Inheritance](https://play.kotlinlang.org/byExample/01_introduction/07_Inheritance)
  */
-
-open class Dog {                // 1
-    open fun sayHello() {       // 2
+//toda classe padrão no kotlin é final
+open class Dog {                // 1 Criando uuma classe pai, uma classe abertta
+    open fun sayHello() {       // 2  Funcão aberta
         println("wow wow!")
     }
 }
-
-class Yorkshire : Dog() {       // 3
-    override fun sayHello() {   // 4
+//forma de ler: Yorshire é um cachorro
+class Yorkshire : Dog() {       // 3 a class Yorshire ta herdando as caracteristicas e comportamentos da classe Dog
+    override fun sayHello() {   // 4 sobreescrevendo a função sayHello
         println("wif wif!")
     }
 }
