de71c04ddc3573a3d7246d3e0531d0020c409057
SnkVictorr <99307258+SnkVictorr@users.noreply.github.com>
2022-12-27 21:53:49 -0300

    Update exemplo2.kt

--- 3_colecoes/5_map_function/exemplo2.kt
+++ 3_colecoes/5_map_function/exemplo2.kt
@@ -9,9 +9,9 @@ fun main() {
     val fruitsBag = listOf("apple","orange","banana","grapes")  // 1
     val clothesBag = listOf("shirts","pants","jeans")           // 2
     val cart = listOf(fruitsBag, clothesBag)                    // 3
-    val mapBag = cart.map { it }                                // 4
-    val flatMapBag = cart.flatMap { it }                        // 5
+    val mapBag = cart.map { it }                                // 4 Mantem as estruturas das listas
+    val flatMapBag = cart.flatMap { it }                        // 5 Junta todas as listas/elementos
 
-    println("Your bags are: $mapBag")
+    println("Your bags are: $mapBag") 
     println("The things you bought are: $flatMapBag")
 }
