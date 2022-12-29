3349bdf1336e8803a1ed0eb944a5a8b10134ee5b
SnkVictorr <99307258+SnkVictorr@users.noreply.github.com>
2022-12-28 23:33:45 -0300

    Update exemplo1.kt

--- 4_poo/2_data_classes/exemplo1.kt
+++ 4_poo/2_data_classes/exemplo1.kt
@@ -3,10 +3,10 @@
  *
  * @see [Data Classes](https://play.kotlinlang.org/byExample/03_special_classes/01_Data%20classes)
  */
-
+//classe para marmazenar valores
 data class User(val name: String, val id: Int) {           // 1
     override fun equals(other: Any?) =
-        other is User && other.id == this.id               // 2
+        other is User && other.id == this.id               // 2 Nào gostaria de fazer a comparação de todas as propriedas do tipo user
 }
 fun main() {
     val user = User("Alex", 1)
@@ -19,16 +19,16 @@ fun main() {
     println("user == thirdUser: ${user == thirdUser}")
 
     // hashCode() function
-    println(user.hashCode())                               // 5
+    println(user.hashCode())                               // 5 pega as propriedades que existem dentro da estrutura de dados e gera um valor
     println(secondUser.hashCode())
     println(thirdUser.hashCode())
 
     // copy() function
-    println(user.copy())                                   // 6
+    println(user.copy())                                   // 6 copiando a estrutura pra outro endereço de memória
     println(user === user.copy())                          // 7
-    println(user.copy("Max"))                              // 8
+    println(user.copy("Max"))                              // 8 copiando novos paramentros
     println(user.copy(id = 3))                             // 9
 
-    println("name = ${user.component1()}")                 // 10
+    println("name = ${user.component1()}")                 // 10 imprimindo os componentes
     println("id = ${user.component2()}")
 }
