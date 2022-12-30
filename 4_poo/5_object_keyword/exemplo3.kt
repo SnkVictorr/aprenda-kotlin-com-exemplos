cd9c8c676837977cd4afb81c7d7334e87bf1a66c
SnkVictorr <99307258+SnkVictorr@users.noreply.github.com>
2022-12-30 16:29:45 -0300

    Update exemplo3.kt

--- 4_poo/5_object_keyword/exemplo3.kt
+++ 4_poo/5_object_keyword/exemplo3.kt
@@ -3,7 +3,7 @@
  *
  * @see [Object Keyword](https://play.kotlinlang.org/byExample/03_special_classes/04_Object)
  */
-
+//declarar uma classe como objeto
 object DoAuth {                                                 //1 
     fun takeParams(username: String, password: String) {        //2 
         println("input Auth parameters = $username:$password")
