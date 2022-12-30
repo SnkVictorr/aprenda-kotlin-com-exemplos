4c2e88a9088d7b9273b5dcb7bfc3b1926da1684b
SnkVictorr <99307258+SnkVictorr@users.noreply.github.com>
2022-12-30 15:16:49 -0300

    Update exemplo2.kt

--- 4_poo/3_enum_classes/exemplo2.kt
+++ 4_poo/3_enum_classes/exemplo2.kt
@@ -10,13 +10,13 @@ enum class Color(val rgb: Int) {                      // 1
     BLUE(0x0000FF),
     YELLOW(0xFFFF00);
 
-    fun containsRed() = (this.rgb and 0xFF0000 != 0)  // 3
+    fun containsRed() = (this.rgb and 0xFF0000 != 0)  // 3 função que serve pra ver se quem ta chamando esse metodo tem a cor vvermelha
 }
 
 fun main() {
     val red = Color.RED
-    println(red)                                      // 4
-    println(red.containsRed())                        // 5
-    println(Color.BLUE.containsRed())                 // 6
-    println(Color.YELLOW.containsRed())               // 7
+    println(red)                                      // 4 RED
+    println(red.containsRed())                        // 5 true
+    println(Color.BLUE.containsRed())                 // 6 false
+    println(Color.YELLOW.containsRed())               // 7 true
 }
