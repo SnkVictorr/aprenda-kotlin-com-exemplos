082fe9a5b744bdb39d57e91f6b0abb06b752a0ad
SnkVictorr <99307258+SnkVictorr@users.noreply.github.com>
2022-12-30 18:36:22 -0300

    Update exemplo1.kt

--- 6_funcoes_avancadas/1_infix_functions/exemplo1.kt
+++ 6_funcoes_avancadas/1_infix_functions/exemplo1.kt
@@ -7,21 +7,21 @@
 fun main() {
 
   infix fun Int.times(str: String) = str.repeat(this)        // 1
-  println(2 times "Bye ")                                    // 2
+  println(2 times "Bye ")                                    // 2 a sa;ida vai ser Bye Bye
 
-  val pair = "Ferrari" to "Katrina"                          // 3
+  val pair = "Ferrari" to "Katrina"                          // 3 cria um par
   println(pair)
 
-  infix fun String.onto(other: String) = Pair(this, other)   // 4
+  infix fun String.onto(other: String) = Pair(this, other)   // 4 tbm cria um par
   val myPair = "McLaren" onto "Lucas"
   println(myPair)
 
   val sophia = Person("Sophia")
   val claudia = Person("Claudia")
-  sophia likes claudia                                       // 5
+  sophia likes claudia                //claudia esta sendo adicionada                       // 5
 }
 
 class Person(val name: String) {
   val likedPeople = mutableListOf<Person>()
-  infix fun likes(other: Person) { likedPeople.add(other) }  // 6
+  infix fun likes(other: Person) { likedPeople.add(other) }  // 6 /adicionando pessoas
 }
