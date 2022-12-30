b45bea556268df709b09ca582857d5442b0b7988
SnkVictorr <99307258+SnkVictorr@users.noreply.github.com>
2022-12-30 16:20:13 -0300

    Update exemplo1.kt

--- 4_poo/4_sealed_classes/exemplo1.kt
+++ 4_poo/4_sealed_classes/exemplo1.kt
@@ -3,13 +3,13 @@
  *
  * @see [Sealed Classes](https://play.kotlinlang.org/byExample/03_special_classes/03_Sealed%20Classes)
  */
-
-sealed class Mammal(val name: String)                                                   // 1
-
+//Cria restrição na classe á nivel de pacote
+sealed class Mammal(val name: String)           ///mamifero                                        // 1
+//elas estão na mesma estrutura de pacote
 class Cat(val catName: String) : Mammal(catName)                                        // 2
 class Human(val humanName: String, val job: String) : Mammal(humanName)
 
-fun greetMammal(mammal: Mammal): String {
+fun greetMammal(mammal: Mammal): String { //função generica que usa mamiferos como tipo
     when (mammal) {                                                                     // 3
         is Human -> return "Hello ${mammal.name}; You're working as a ${mammal.job}"    // 4
         is Cat -> return "Hello ${mammal.name}"                                         // 5     
@@ -17,5 +17,9 @@ fun greetMammal(mammal: Mammal): String {
 }
 
 fun main() {
-    println(greetMammal(Cat("Snowy")))
+    val cat: Mammal = Cat("Snowy") //inferencia de tipos 
+    val human = Human("VVenilton, "Líder técnico na DIO")
+                              
+    println(greetMammal(cat))
+    println(greetMammal(human))
 }
