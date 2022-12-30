5c3bb669fda5ca8253a61770a591c75a224a20ef
SnkVictorr <99307258+SnkVictorr@users.noreply.github.com>
2022-12-30 17:17:02 -0300

    Update exemplo1.kt

--- 5_funcoes_de_escopo/1_let/exemplo1.kt
+++ 5_funcoes_de_escopo/1_let/exemplo1.kt
@@ -3,7 +3,7 @@
  *
  * @see [let](https://play.kotlinlang.org/byExample/06_scope_functions/01_let)
  */
-
+//let pode ser usado para escopo e para verificação de nulls
 fun customPrint(s: String) {
     print(s.uppercase())
 }
@@ -11,8 +11,8 @@ fun customPrint(s: String) {
 fun main() {
 
     val empty = "test".let {               // 1
-        customPrint(it)                    // 2
-        it.isEmpty()                       // 3
+        customPrint(it)                    // 2 it é "test"
+        it.isEmpty()                       // 3 verifica se é vazio
     }
     println(" is empty: $empty")
 
@@ -20,15 +20,15 @@ fun main() {
     fun printNonNull(str: String?) {
         println("Printing \"$str\":")
 
-        str?.let {                         // 4
-            print("\t")
-            customPrint(it)
+        str?.let {                         // 4 se str for nulo, let n será executado
+            print("\t") //tabulação
+            customPrint(it) //imprimindo em letra maiuscula
             println()
         }
     }
     
     fun printIfBothNonNull(strOne: String?, strTwo: String?) {
-        strOne?.let { firstString ->       // 5 
+        strOne?.let { firstString ->       // 5 usando custom names e lets aninnhados
             strTwo?.let { secondString ->
                 customPrint("$firstString : $secondString")
                 println()
