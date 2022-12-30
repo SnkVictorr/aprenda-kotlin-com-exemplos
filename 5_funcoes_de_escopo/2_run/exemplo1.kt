68ea228df2cbd7cee4cf759f9e0421e6c1e16fc1
SnkVictorr <99307258+SnkVictorr@users.noreply.github.com>
2022-12-30 17:26:18 -0300

    Update exemplo1.kt

--- 5_funcoes_de_escopo/2_run/exemplo1.kt
+++ 5_funcoes_de_escopo/2_run/exemplo1.kt
@@ -8,10 +8,10 @@ fun main() {
 
     fun getNullableLength(ns: String?) {
         println("for \"$ns\":")
-        ns?.run {                                                  // 1
+        ns?.run {                                                  // 1 //run tbm verifica se a nulabilidade
             println("\tis empty? " + isEmpty())                    // 2
             println("\tlength = $length")                           
-            length                                                 // 3
+            length                                                 // 3 retorna length
         }
     }
     getNullableLength(null)
