04b702c7bb01c22fe77a9438e777886a1b902eda
SnkVictorr <99307258+SnkVictorr@users.noreply.github.com>
2022-12-30 18:14:24 -0300

    Update exemplo1.kt

--- 5_funcoes_de_escopo/4_apply/exemplo1.kt
+++ 5_funcoes_de_escopo/4_apply/exemplo1.kt
@@ -5,17 +5,17 @@
  */
 
 data class Person(var name: String, var age: Int, var about: String) {
-    constructor() : this("", 0, "")
+    constructor() : this("", 0, "") //Person vai ser inicializado com esses parametros
 }
 
 fun main() {
 
     val jake = Person()                                     // 1
-    val stringDescription = jake.apply {                    // 2
+    val stringDescription = jake.apply {      //modificando a classe , atualizando valores            // 2
         name = "Jake"                                       // 3
         age = 30
         about = "Android developer"
-    }.toString()                                            // 4
+    }.toString() //podemos aplicar uma operação no final                                            // 4
 
     println(stringDescription)
 }
