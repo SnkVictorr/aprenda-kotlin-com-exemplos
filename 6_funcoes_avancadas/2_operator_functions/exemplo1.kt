24a8b9d63fc215d4ca4e83ef93bac6e50a7ce579
SnkVictorr <99307258+SnkVictorr@users.noreply.github.com>
2023-01-02 17:21:04 -0300

    Update exemplo1.kt

--- 6_funcoes_avancadas/2_operator_functions/exemplo1.kt
+++ 6_funcoes_avancadas/2_operator_functions/exemplo1.kt
@@ -7,10 +7,10 @@
 fun main() {
 
   operator fun Int.times(str: String) = str.repeat(this)       // 1
-  println(2 * "Bye ")                                          // 2
+  println(2 * "Bye ")        //um inteiro e uma string, times virou *                                  // 2
 
   operator fun String.get(range: IntRange) = substring(range)  // 3
   val str = "Always forgive your enemies; nothing annoys them so much."
-  println(str[0..14])                                          // 4
+  println(str[0..14])                                          // 4 imprindo a string de 0 a 14 caracteres
 
 }
