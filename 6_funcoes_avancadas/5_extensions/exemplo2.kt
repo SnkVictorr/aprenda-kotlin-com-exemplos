8663c90008e4fe9816bda0491cd763cfc0273961
SnkVictorr <99307258+SnkVictorr@users.noreply.github.com>
2023-01-02 19:38:55 -0300

    Update exemplo2.kt

--- 6_funcoes_avancadas/5_extensions/exemplo2.kt
+++ 6_funcoes_avancadas/5_extensions/exemplo2.kt
@@ -3,8 +3,8 @@
  *
  * @see [Extension Functions and Properties](https://play.kotlinlang.org/byExample/04_functional/03_extensionFunctions)
  */
-
-fun <T> T?.nullSafeToString() = this?.toString() ?: "NULL"  // 1
+//recebendo T. Se this não for nulo ele vai fazer o toString, se não ele vai imprimeir "Ausencia de valor"
+fun <T> T?.nullSafeToString() = this?.toString() ?: "Ausencia de valor"  // 1
 
 fun main() {
     println(null.nullSafeToString())
