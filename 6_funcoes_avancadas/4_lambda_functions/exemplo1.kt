86e7f3497fe1961670c7e6bec7f8116b3f44783b
SnkVictorr <99307258+SnkVictorr@users.noreply.github.com>
2023-01-02 18:46:51 -0300

    Update exemplo1.kt

--- 6_funcoes_avancadas/4_lambda_functions/exemplo1.kt
+++ 6_funcoes_avancadas/4_lambda_functions/exemplo1.kt
@@ -9,17 +9,17 @@ fun main() {
     // All examples create a function object that performs upper-casing.
     // So it's a function from String to String
 
-    val upperCase1: (String) -> String = { str: String -> str.uppercase() } // 1
+    val upperCase1: (String) -> String = { str: String -> str.uppercase() } // 1 especificando internnamente e externamente
 
-    val upperCase2: (String) -> String = { str -> str.uppercase() }         // 2
+    val upperCase2: (String) -> String = { str -> str.uppercase() }         // 2 n é necerrário definir o tipo externamente, pois ja é definindo internamente
 
-    val upperCase3 = { str: String -> str.uppercase() }                     // 3
+    val upperCase3 = { str: String -> str.uppercase() }                     // 3 definindo só externamente
 
-    // val upperCase4 = { str -> str.uppercase() }                          // 4
+    // val upperCase4 = { str -> str.uppercase() }                          // 4 é necessário definir o tipo pelo menos internamente ou externamente, se não, Nào funcionara
 
-    val upperCase5: (String) -> String = { it.uppercase() }                 // 5
+    val upperCase5: (String) -> String = { it.uppercase() }                 // 5 it substitui apenas 1 parametro
 
-    val upperCase6: (String) -> String = String::uppercase                  // 6
+    val upperCase6: (String) -> String = String::uppercase                  // 6 método que usa uma string, e a transforma em maiusculas
 
     println(upperCase1("hello"))
     println(upperCase2("hello"))
